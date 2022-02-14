9c07a3e3bffc92ea83ec0def96c5c7ee6825bb61
vincent.hou <vincenthou365@gmail.com>
2022-02-14 19:21:46 +0800

    use URL parameter

--- js/index.js
+++ js/index.js
@@ -24,7 +24,8 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('|#countdown 3||某|某|祝|你|生|日|快|乐|#rectangle|');
+      var word = decodeURIComponent(location.search.slice(6)).split('').join('|')
+      S.UI.simulate('|#countdown 3||'+ word +'|#rectangle|');
     }
 
     S.Drawing.loop(function () {
